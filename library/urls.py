9ce16984d388509a36bfe0d5e7111cf3f888304f
tmyrcell <danila.stepanidenko@mail.ru>
2021-02-25 18:44:39 +0300

    2021-02-25 18:44:30 v.1.2

--- library/urls.py
+++ library/urls.py
@@ -6,5 +6,12 @@ urlpatterns = [
     path('', views.library_home, name='library_home'),
     url(r'^books/$', views.BookListView.as_view(), name='books'),
     url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
-    url(r'^book/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    # path('authors/', views.AuthorListView.as_view(), name='authors'),
+    # path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+    url(r'^mybook/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    url(r'^borrowed/$', views.LoanedBooksAllUserListView.as_view(), name='all-borrowed'),
+]
+
+urlpatterns += [
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
 ]
--- library/views.py
+++ library/views.py
@@ -1,8 +1,12 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.http import Http404, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
 from .models import Book, Author, Language, BookInstance, Genre
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse
+import datetime
+from library.forms import RenewBookForm
+from django.contrib.auth.decorators import permission_required, login_required
 
 
 # Create your views here.
@@ -24,13 +28,6 @@ def library_home(request):
 class BookListView(generic.ListView):
     model = Book
     paginate_by = 10
-
-    def get_context_data(self, **kwargs):
-        # В первую очередь получаем базовую реализацию контекста
-        context = super(BookListView, self).get_context_data(**kwargs)
-        # Добавляем новую переменную к контексту и иниуиализируем ее некоторым значением
-        context['some_data'] = 'This is just some data'
-        return context
 
 
 class BookDetailView(generic.DetailView):
@@ -55,3 +52,52 @@ class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
 
     def get_queryset(self):
         return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+
+
+class LoanedBooksAllUserListView(LoginRequiredMixin, generic.ListView):
+    """
+    Generic class-based view listing books on loan to current user.
+    """
+    model = BookInstance
+    permission_required = 'library.can_mark_returned'
+    template_name = 'library/bookinstance_list_borrowed_all.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(status__exact='o').order_by('due_back')
+
+
+
+@login_required
+@permission_required('library.can_mark_returned', raise_exception=True)
+def renew_book_librarian(request, pk):
+    """
+    View function for renewing a specific BookInstance by librarian
+    """
+    book_inst = get_object_or_404(BookInstance, pk=pk)
+
+    # If this is a POST request then process the Form data
+    if request.method == 'POST':
+
+        # Create a form instance and populate it with data from the request (binding):
+        form = RenewBookForm(request.POST)
+
+        # Check if the form is valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
+            book_inst.due_back = form.cleaned_data['renewal_date']
+            book_inst.save()
+
+            # redirect to a new URL:
+            return HttpResponseRedirect(reverse('/'))
+
+    # If this is a GET (or any other method) create the default form.
+    else:
+        proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+        form = RenewBookForm(initial={'renewal_date': proposed_renewal_date, })
+
+    context = {
+        'form': form,
+        'book_instance': book_inst,
+    }
+    return render(request, 'library/book_renew_librarian.html', context)
